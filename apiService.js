c301866f8c914848e4bb256b63e34c8de545a2fb
Sylvie <sylvie.z.lu@gmail.com>
2020-05-11 23:18:33 +0200

    Trending topRated done

--- apiService.js
+++ apiService.js
@@ -1,45 +1,6 @@
 const APÎ_URL = "https://api.themoviedb.org/3/movie/";
 const API_KEY = "2997c5290abfb556adf35db19e36cc28";
 
-//1
-// export function fetchMovie(movieId, cb) {
-//     const url = `https://api.themoviedb.org/3/movie/99?api_key=617f779f9850bfb3355824689e154920&language=fr-FR%27`;
-
-//     var xhr_object = new XMLHttpRequest();
-//     xhr_object.open("GET", url, false);
-//     xhr_object.send(null);
-
-//     if (xhr_object.readyState == 4) {
-//         return cb(JSON.parse(xhr_object.responseText));
-//     }
-
-// }
-
-//2
-// export default function fetchMovie(movieId) {
-//     const url = `https://api.themoviedb.org/3/movie/99?api_key=617f779f9850bfb3355824689e154920&language=fr-FR%27`;
-
-//     return new Promise((resolve, reject) => {
-//         var xhr_object = new XMLHttpRequest();
-//         xhr_object.open("GET", url, false);
-//         xhr_object.send(null);
-
-//         if (xhr_object.readyState == 4) {
-//             resolve(JSON.parse(xhr_object.responseText));
-//         }
-//     });
-// }
-
-// export default function fetchMovie(movieId) {
-//     const url = ``;
-//     return fetch(url).then(res => {
-//         return res.json()
-//     }).then(movie => {
-//         return movie;
-//     })
-// }
-
-// 3
 export async function fetchMovie(movieId) {
     const url = `https://api.themoviedb.org/3/movie/99?api_key=617f779f9850bfb3355824689e154920&language=fr-FR%27`;
     let res = await fetch(url);
@@ -52,12 +13,22 @@ export async function fetchNetflixOriginals() {
     const url = `https://api.themoviedb.org/3/discover/tv?api_key=617f779f9850bfb3355824689e154920&with_networks=213`;
     let res = await fetch(url);
     let movies = await res.json();
-   return movies;
+    return movies;
     
 }
 
-export function fetchTrending() {}
+export async function fetchTrending() {
+    const url = `https://api.themoviedb.org/3/trending/all/day?api_key=617f779f9850bfb3355824689e154920`
+    let res = await fetch(url);
+    let trendingNow = await res.json();
+    return trendingNow;
+}
 
-export function fetchTopRated() {}
+export async function fetchTopRated() {
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=617f779f9850bfb3355824689e154920&page=1`
+    let res = await fetch(url);
+    let topRated = await res.json();
+    return topRated;
+}
 
 export function fetchByGenreMovies(genre) {}
--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-import {fetchMovie, fetchNetflixOriginals}  from "./apiService.js";
+import {fetchMovie, fetchNetflixOriginals, fetchTrending, fetchTopRated}  from "./apiService.js";
 import Header from "./components/Header.mjs";
 
-// (() => {
-//     //Callback
-//     const getResponse = (data) => {
-//         return data;
-//     };
-//     try {
-//         let movie = fetchMovie(157336, getResponse);
-//         console.log(movie);
-//         document.getElementById("header").innerHTML = Header(movie);
-//         document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
-//     } catch (e) {
-//         console.log(e)
-//     }
-
-//     console.log(fetchMovie)
-// })();
-
-
-
-//2
-// (() => {
-//     let movie = fetchMovie(157336).then(movie => {
-//         console.log(movie)
-//         document.getElementById("header").innerHTML = Header(movie);
-//         document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
-//     })
-
-// })();
-
-// 3
 (async() => {
     let movie = await fetchMovie(157336);
     //console.log(movie)
     document.getElementById("header").innerHTML = Header(movie);
     document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
 
-    let movies = await fetchNetflixOriginals();
-    movies = movies.results
+    let moviesOriginals = await fetchNetflixOriginals();
+    moviesOriginals = moviesOriginals.results
 
     let netflixOriginals = document.getElementsByClassName("movies__container--movie__netflix")[0]
-    console.log(netflixOriginals)
-    for(let i = 0; i < movies.length; i++){
-        
+    // console.log(netflixOriginals)
+    for(let i = 0; i < moviesOriginals.length; i++){
         netflixOriginals.innerHTML += `
-            <img src="https://image.tmdb.org/t/p/original//${movies[i].poster_path}" class="movies__container--movie-image"/>
+            <img src="https://image.tmdb.org/t/p/original//${moviesOriginals[i].poster_path}" class="movies__container--movie-image"/>
             `
-        netflixOriginals.style.overflow = "auto"
-        netflixOriginals.style.display = "flex"
-        console.log(movies[i].poster_path)
+        // console.log(movies[i].poster_path)
     }
+    netflixOriginals.style.overflow = "auto"
+    netflixOriginals.style.display = "flex"
+
+    let moviesTrending = await fetchTrending();
+    moviesTrending = moviesTrending.results
+
+    let moviesTopRated = await fetchTopRated();
+    moviesTopRated = moviesTopRated.results
+
+    let moviesContainer = document.getElementsByClassName("movies__container--movie")
+    let trending = moviesContainer[0]
+    let topRated = moviesContainer[1]
+    // console.log(trending)
+    for(let i = 0; i < moviesTrending.length; i++){
+        console.log(moviesTrending[i])
+        trending.innerHTML += `
+        <img src="https://image.tmdb.org/t/p/original//${moviesTrending[i].backdrop_path}" class="movies__container--movie-image"/>
+        `
+    }
+    trending.style.overflow = "auto"
+    trending.style.display = "flex"
+
+    for(let i = 0; i < moviesTopRated.length; i++){
+        console.log(moviesTopRated[i])
+        topRated.innerHTML += `
+        <img src="https://image.tmdb.org/t/p/original//${moviesTopRated[i].backdrop_path}" class="movies__container--movie-image"/>
+        `
+    }
+    topRated.style.overflow = "auto"
+    topRated.style.display = "flex"
+
 })();
