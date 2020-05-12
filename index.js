69af9539e740be3d3a94dc4fe4f6e4e73478b550
aminehadefdev <amine.hadef.dev@gmail.com>
2020-05-12 23:17:55 +0200

    oppti js

--- index.js
+++ index.js
@@ -5,96 +5,44 @@ import Header from "./components/Header.mjs";
     let movie = await fetchMovie(157336);
     document.getElementById("header").innerHTML = Header(movie);
     document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
+    var o = 0
 
-    let moviesOriginals = await fetchNetflixOriginals();
-    moviesOriginals = moviesOriginals.results
+    async function display(fun, htmlElemnt, typeImg, parmFun = null){
+        let movies = await fun(parmFun)
+        movies = movies.results
 
-    let netflixOriginals = document.getElementsByClassName("movies__container--movie__netflix")[0]
-        // récupérer les résultats de la section Netflix Originals
-    for (let i = 0; i < moviesOriginals.length; i++) {
-        netflixOriginals.innerHTML += `
-            <img data-key-id=${moviesOriginals[i].id} src="https://image.tmdb.org/t/p/original//${moviesOriginals[i].poster_path}" class="movies__container--movie-image"/>
-            `
+        htmlElemnt.style.overflow = "auto"
+        htmlElemnt.style.display = "flex"
+
+        if(typeImg == 'poster'){
+            for(let i = 0; i < movies.length; i++){
+                htmlElemnt.innerHTML += `
+                    <img data-key-id=${movies[i].id} src="https://image.tmdb.org/t/p/original//${movies[i].poster_path}" class="movies__container--movie-image"/>
+                `
+            }
+        }else if(typeImg == 'backdrop'){
+            for(let i = 0; i < movies.length; i++){
+                htmlElemnt.innerHTML += `
+                    <img data-key-id=${movies[i].id} src="https://image.tmdb.org/t/p/original//${movies[i].backdrop_path}" class="movies__container--movie-image"/>
+                `
+            }
+        }
+        o++
+        if(o >= 6){
+            let images = Array.from(document.querySelectorAll('.movies__container--movie-image'))
+            images.map(function(elm){
+                elm.addEventListener('click',function(){
+                    let id = this.getAttribute('data-key-id')
+                    console.log(id)
+                })
+            })
+        }
     }
-    netflixOriginals.style.overflow = "auto"
-    netflixOriginals.style.display = "flex"
 
-    let moviesTrending = await fetchTrending();
-    moviesTrending = moviesTrending.results
-
-    let moviesTopRated = await fetchTopRated();
-    moviesTopRated = moviesTopRated.results
-
-    let movieGenreActions = await fetchByGenreMovies(28)
-    movieGenreActions = movieGenreActions.results
-
-    let movieGenreComedy = await fetchByGenreMovies(35)
-    movieGenreComedy = movieGenreComedy.results
-
-    let movieGenreDocumentary = await fetchByGenreMovies(99)
-    movieGenreDocumentary = movieGenreDocumentary.results
-
-    let moviesContainer = document.getElementsByClassName("movies__container--movie")
-    let trending = moviesContainer[0]
-    let topRated = moviesContainer[1]
-    let actionsMovie = moviesContainer[2]
-    let comediesMovie = moviesContainer[3]
-    let documentaryMovie = moviesContainer[4]
-
-    // récupérer les résultats de la section "Trending Now"
-    for (let i = 0; i < moviesTrending.length; i++) {
-        trending.innerHTML += `
-        <img data-key-id=${moviesTrending[i].id} src="https://image.tmdb.org/t/p/original//${moviesTrending[i].backdrop_path}" class="movies__container--movie-image"/>
-        `
-    }
-    trending.style.overflow = "auto"
-    trending.style.display = "flex"
-
-    // récupérer les résultats de la section "Top Rated"
-    for (let i = 0; i < moviesTopRated.length; i++) {
-        topRated.innerHTML += `
-        <img data-key-id=${moviesTopRated[i].id} src="https://image.tmdb.org/t/p/original//${moviesTopRated[i].backdrop_path}" class="movies__container--movie-image"/> `
-    }
-    topRated.style.overflow = "auto"
-    topRated.style.display = "flex"
-
-    // récupérer les résultats des films de genre Action
-    for (let i = 0; i < movieGenreActions.length; i++) {
-        // console.log(movieGenreActions)
-        actionsMovie.innerHTML += ` 
-        <img data-key-id=${movieGenreActions[i].id} src="https://image.tmdb.org/t/p/original//${movieGenreActions[i].backdrop_path}" class="movies__container--movie-image"/>`
-    }
-    actionsMovie.style.overflow = "auto"
-    actionsMovie.style.display = "flex"
-
-    // récupérer les résultats des films de genre Comedy
-    for (let i = 0; i < movieGenreComedy.length; i++) {
-        // console.log(movieGenreComedy)
-        comediesMovie.innerHTML += ` 
-        <img data-key-id=${movieGenreComedy[i].id} src="https://image.tmdb.org/t/p/original//${movieGenreComedy[i].backdrop_path}" class="movies__container--movie-image"/>`
-    }
-    comediesMovie.style.overflow = "auto"
-    comediesMovie.style.display = "flex"
-
-    // récupérer les résultats des films de genre Documentary
-    for (let i = 0; i < movieGenreDocumentary.length; i++) {
-        // console.log(movieGenreDocumentary)
-        documentaryMovie.innerHTML += ` 
-        <img data-key-id=${movieGenreDocumentary[i].id} src="https://image.tmdb.org/t/p/original//${movieGenreDocumentary[i].backdrop_path}" class="movies__container--movie-image"/>`
-    }
-    documentaryMovie.style.overflow = "auto"
-    documentaryMovie.style.display = "flex"
-
-    let images = Array.from(document.querySelectorAll('.movies__container--movie-image'))
-        // console.log(images)
-
-    images.map(elem => {
-        elem.addEventListener('click', async function() {
-            let id = this.getAttribute("data-key-id")
-            let movie = await fetchMovie(id).then(result => result)
-
-        })
-    })
-
-
+    display(fetchNetflixOriginals, document.getElementsByClassName("movies__container--movie__netflix")[0], 'poster')
+    display(fetchTrending, document.getElementsByClassName("movies__container--movie")[0], 'backdrop')
+    display(fetchTopRated, document.getElementsByClassName("movies__container--movie")[1], 'backdrop')
+    display(fetchByGenreMovies, document.getElementsByClassName("movies__container--movie")[2], 'backdrop', 28)
+    display(fetchByGenreMovies, document.getElementsByClassName("movies__container--movie")[3], 'backdrop', 35)
+    display(fetchByGenreMovies, document.getElementsByClassName("movies__container--movie")[4], 'backdrop', 99)
 })();
